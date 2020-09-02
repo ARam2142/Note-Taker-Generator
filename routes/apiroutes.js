e16aece2c2497774120a250c3b63313587456444
Aram2142 <andresaaramirez@aol.com>
2020-09-02 00:26:32 -0400

    hmtlroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,1 +1,45 @@
-//make routes for GET POST and DELETE
+//make routes for GET POST and DELETE notes
+const fs = require('fs');
+const db = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = function(app) {
+    //GET  api
+    //reads the db.json file and returns saved notes in json
+    app.get('/api/notes', function (req, res) {
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) {
+                throw err;
+                res.send(db)
+            }
+        })
+        res.json(db)
+    })
+
+
+
+
+    //POST api
+
+
+
+
+
+    //DELETE api
+
+
+
+}
+
+//The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
+
+//The following API routes should be created:
+//GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+
+//POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
+//DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. 
+
+//This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, 
+
+//you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Series of npm packages that we will use to give our server useful functionality
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //routes
-//require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 //listens to the port
 app.listen(PORT, function() {
