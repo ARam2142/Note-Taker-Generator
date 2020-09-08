99c36919f1d03cef92c91ef12db72edb977b067f
Aram2142 <andresaaramirez@aol.com>
2020-09-07 21:17:05 -0400

    added uuid

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const db = require('../db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     //GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-    app.get("/api/notes", function(req, res) {
+    app.get("/api/notes", function (req, res) {
         console.log('get')
         fs.readFile('./db/db.json', (err, data) => {
             if (err) throw err;
@@ -14,7 +14,7 @@ module.exports = function(app) {
             res.json(JSON.parse(data));
         });
     });
-      
+
     //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
     app.post('/api/notes', function (req, res) {
         console.log("post");
@@ -28,34 +28,33 @@ module.exports = function(app) {
         });
         res.json(db);
     })
-    
+
     //DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. 
     //This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, 
     //you'll need to read all notes from the db.json file, remove the note with the given id property, 
     //and then rewrite the notes to the db.json file.
-    app.delete('/api/notes/:id', function(req, res) {
+    app.delete('/api/notes/:id', function (req, res) {
 
         //works cited: https://stackoverflow.com/questions/53661683/matching-a-delete-request-to-a-json-object
         fs.readFile('./db/db.json', (err, data) => {
-        if (err) throw err;
-        
-        let deletedNotes = req.params.id;
-        for (let i = 0; i < db.length; i++) {   
-            if (db[i].id === deletedNotes) {
-                db.splice(i, 1);
-                res.json(db);
-                
-                fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
-                    if (err) throw err;
-                
-                });
-                
-            }  
-            
-        }
-    });
-    
-});
+            if (err) throw err;
 
+            let deletedNotes = req.params.id;
+            for (let i = 0; i < db.length; i++) {
+                if (db[i].id === deletedNotes) {
+                    db.splice(i, 1);
+                    res.json(db);
+
+                    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+                        if (err) throw err;
+
+                    });
+
+                }
+
+            }
+        });
+
+    });
 
 }
